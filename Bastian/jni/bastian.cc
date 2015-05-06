b15ad25a7f703c7d9f32dd4de8aee2ce73566175
David Corticchiato <david.corticchiato@iscool-e.com>
2015-05-06 13:27:59 +0200

    add v8 scope init in jni source

--- Bastian/jni/bastian.cc
+++ Bastian/jni/bastian.cc
@@ -1,9 +1,15 @@
 #include <jni.h>
 #include <android/log.h>
+#include <v8.h>
+
+using namespace v8;
 
 
+extern "C" JNIEXPORT void JNICALL Java_com_github_phantasien_bastian_BastianMainActivity_callMyName(JNIEnv* env, jobject thiz) {
 
+  // Create a new Isolate and make it the current one.
+  Isolate* isolate = Isolate::New();
+  Isolate::Scope isolate_scope(isolate);
 
-extern "C" JNIEXPORT void JNICALL Java_com_github_bastian_BastianMainActivity_callMyName(JNIEnv* env, jobject thiz) {
-    __android_log_write(ANDROID_LOG_INFO, "Bastian", "Moon Child !");
+  __android_log_write(ANDROID_LOG_INFO, "Bastian", "Moon Child !");
 }
