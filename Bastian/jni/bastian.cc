d6f2ab248119fc0dff7d231c00702ac537f6e879
David Corticchiato <david.corticchiato@iscool-e.com>
2015-05-06 15:13:50 +0200

    close #3: sweet, this is how you do a simple calculation with v8. Next question ?

--- Bastian/jni/bastian.cc
+++ Bastian/jni/bastian.cc
@@ -2,14 +2,38 @@
 #include <android/log.h>
 #include <v8.h>
 
+#include <sstream>
+
 using namespace v8;
 
 
 extern "C" JNIEXPORT void JNICALL Java_com_github_phantasien_bastian_BastianMainActivity_callMyName(JNIEnv* env, jobject thiz) {
 
+  const char * rawSource = "1 + 1";
+  
   // Create a new Isolate and make it the current one.
   Isolate* isolate = Isolate::New();
   Isolate::Scope isolate_scope(isolate);
 
-  __android_log_write(ANDROID_LOG_INFO, "Bastian", "Moon Child !");
+  HandleScope handle_scope(isolate);
+
+  Handle<ObjectTemplate> global = ObjectTemplate::New(isolate);
+
+  // Create a new context.
+  Local<Context> context = Context::New(isolate, NULL, global);
+
+  // Enter the context for compiling and running the hello world script.
+  Context::Scope context_scope(context);
+
+  Local<String> source = String::NewFromUtf8(isolate, rawSource);
+  Local<Script> script = Script::Compile(source);
+
+  Local<Value> result = script->Run();
+
+  std::stringstream stream;
+  std::string logValue;
+
+  stream << result->NumberValue();
+
+  __android_log_write(ANDROID_LOG_INFO, "Bastian", stream.str().c_str());
 }
